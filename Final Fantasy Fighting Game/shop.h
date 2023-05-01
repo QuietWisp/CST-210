8299cae78686f97abe520d9407c9bd815a102791
Austin Fisher <46381860+QuietWisp@users.noreply.github.com>
2023-04-30 23:44:42 -0700

    Create shop.h

---
+++ Final Fantasy Fighting Game/shop.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "enemy.h"
+#include "player.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shop : public Player, public Enemy {
+private:
+  string
+      buy; // string for storing whether or not the user wants to buy something
+  string decision; // string for storing what the user wants to buy
+
+public:
+  Shop();
+  ~Shop();
+
+  string user; // string for storing the user
+
+  int shopLvl = 0; // int for shop level
+  int cash = 0;    // int for cash
+  int potions = 0; // int for amount of potions
+  int o = 0;
+
+  int enemyTracker = 99999999; // int tracking which enemy and its hp
+  bool hasBeenKilled =
+      false; // bool for whether or not the enemy at each level has been killed,
+             // used to determine whether or not a user can but a level upgrade
+
+  void initialize();
+  void loadShop(int, int, int, int);
+
+  void setCash(int);
+  void getCash();
+  void setInitals();
+  void addCash(int);
+
+  void killed();
+
+  void addPotion();
+  void removePotion();
+
+  void display();
+};

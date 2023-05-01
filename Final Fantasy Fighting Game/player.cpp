1d5725e76d08ea99410ee77b0663630bfbecf990
Austin Fisher <46381860+QuietWisp@users.noreply.github.com>
2023-04-30 23:44:27 -0700

    Create player.cpp

---
+++ Final Fantasy Fighting Game/player.cpp
@@ -0,0 +1,12 @@
+#include "player.h"
+#include <iostream>
+using namespace std;
+
+// constructor
+Player::Player() {}
+
+// destructor
+Player::~Player() {}
+
+// function to set player hp
+void Player::setPlayerHP(int hp) { userHP = hp; }
